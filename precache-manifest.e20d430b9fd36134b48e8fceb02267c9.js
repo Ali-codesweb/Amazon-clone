9a2cf87f56e0eaffec4a918bcfdfc54951ccfbf9
Aliasgar <aliasgar1099@gmail.com>
2020-08-29 12:17:04 +0530

    Updates

---
+++ precache-manifest.e20d430b9fd36134b48e8fceb02267c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d04391735d9e3f473eba2af046fccd5",
+    "url": "/Amazon-clone/index.html"
+  },
+  {
+    "revision": "0d7190f02402ee3631de",
+    "url": "/Amazon-clone/static/css/main.0e15b584.chunk.css"
+  },
+  {
+    "revision": "66e46078140b666cdac4",
+    "url": "/Amazon-clone/static/js/2.d4f05974.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Amazon-clone/static/js/2.d4f05974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7190f02402ee3631de",
+    "url": "/Amazon-clone/static/js/main.85410313.chunk.js"
+  },
+  {
+    "revision": "b4ca44839ab564cdfff3",
+    "url": "/Amazon-clone/static/js/runtime-main.a692d69b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/http//:Ali-codesweb.io/Amazon-clone/precache-manifest.bc74ca3540e0e248565c4b4facf2692d.js"
+  "/Amazon-clone/precache-manifest.e20d430b9fd36134b48e8fceb02267c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/http//:Ali-codesweb.io/Amazon-clone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Amazon-clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
